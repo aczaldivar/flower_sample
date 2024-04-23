996c52fab88d33bd454b81dcdd7f589d0b1f5583
Anna Zaldivar <anna.zaldivar@gmail.com>
2024-04-23 10:04:25 -0700

    contact update

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,12 +1,24 @@
+
 import React from 'react';
 import Navbar from '../Components/Navbar.js';
+
 const ContactUs =()=>{
     return(
 <>
 <Navbar/>
-     <h1>Contact Us</h1>
-     <h2>Sample Flowers</h2>
-
+<div>
+    <div class="background">
+      <h1 id="SFtitle">Sample Flowers</h1>
+      </div>
+      <h1 class ="contacttitle">Contact Us</h1>
+      <div>
+      <div id="contact_info">
+        <h2>Address: 1111 Main st. Rosemont,CA 90023</h2> 
+        <h2> Phone: (888)888-8888</h2>
+        <h2> Email: rosemail@mail.com</h2>
+      </div>
+      </div>
+      </div>
 </>
 )};
 
